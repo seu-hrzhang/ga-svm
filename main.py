5e45ea39006f15a683064deae1dbd472b760de8c
张浩然 <henry_hrzhang@icloud.com>
2021-12-13 00:01:54 +0800

    update source files

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-import smo
 from smo import *
 
 
@@ -31,7 +27,7 @@ def get_visual(data, labels, title='Figure'):
     """
     data = np.array(data)
     labels = np.array(labels)
-    [rows, cols] = data.shape
+    rows = data.shape[0]
 
     x = data[:, 0]
     y = data[:, 1]
@@ -50,11 +46,11 @@ def run():
     data, labels = load_data('data.txt')
     get_visual(data, labels, 'Input Data')
 
-    # alpha, b = smo_naive(data, labels, 0.6, 0.001, 40)
-
-    smo_solver = SMO(data, labels, 0.6, 0.001, max_it=100)
-    if smo_solver.run_naive():
+    smo_solver = SMO(data, labels, 0.6, 0.001, max_it=40)
+    if smo_solver.train_naive():
         smo_solver.plot()
+        print(smo_solver.w)
+        print(smo_solver.b)
     else:
         print('SMO process failed')
 
--- smo.py
+++ smo.py
@@ -4,7 +4,7 @@ import random
 import matplotlib.pyplot as plt
 
 
-def get_random_id(i, m):
+def random_var(i, m):
     """
     randomly select variable index 'j'
     :param i: given index 'i' to distinguish from
@@ -15,89 +15,6 @@ def get_random_id(i, m):
     while j == i:
         j = int(random.uniform(0, m))
     return j
-
-
-def smo_naive(data, labels, const, toler, max_iter):
-    """
-    Naive implemantation of SMO algo. Target variables mark randomly.
-    :param data: input data (from 'load_data'), aka variable x
-    :param labels: labels of data (from 'load_data'), aka variable y
-    :param const: constant limit of parameter alpha
-    :param toler:
-    :param max_iter: Maximum iterations
-    :return:
-    """
-    # 'x' and 'y' both columm matrix/vector
-    x = mat(data)
-    y = mat(labels).transpose()
-
-    b = 0.0
-    it = 0  # count of iterations
-    m, n = shape(x)
-    alpha = mat(zeros((m, 1)))
-
-    while it < max_iter:
-        changes = 0
-        for i in range(m):
-            g_xi = float(multiply(alpha, y).T * (x * x[i, :].T)) + b
-            e_i = g_xi - float(y[i])
-
-            if y[i] * e_i < -toler and alpha[i] < const or y[i] * e_i > toler and alpha[i] > 0:
-                j = get_random_id(i, m)
-                g_xj = float(multiply(alpha, y).T * (x * x[j, :].T)) + b
-                e_j = g_xj - float(y[j])
-
-                # record old values of 'alpha_i' and 'alpha_j'
-                alpha_io = alpha[i].copy()
-                alpha_jo = alpha[j].copy()
-
-                if y[i] != y[j]:
-                    l_lim = max(0, alpha_jo - alpha_io)
-                    h_lim = min(const, const + alpha_jo - alpha_io)
-                else:
-                    l_lim = max(0, alpha_io + alpha_jo - const)
-                    h_lim = min(const, alpha_io + alpha_jo)
-
-                if l_lim == h_lim:
-                    # print('limits overlapped')
-                    continue
-
-                # k_12 = k_21
-                k_11 = x[i, :] * x[i, :].T
-                k_12 = x[i, :] * x[j, :].T
-                k_22 = x[j, :] * x[j, :].T
-                eta = k_11 + k_22 - 2.0 * k_12
-
-                # alpha[j] > l_lim, alpha[j] < h_lim
-                alpha_jn = max(min((alpha_jo + y[j] * (e_i - e_j) / eta), h_lim), l_lim)  # restrict range of 'alpha[j]'
-                alpha[j] = alpha_jn
-
-                if abs(alpha_jn - alpha_jo < 0.00001):
-                    # print('little change in alpha[j]')
-                    continue
-
-                alpha_in = alpha_io + y[j] * y[i] * (alpha_jo - alpha_jn)
-                alpha[i] = alpha_in
-
-                b_i = b - e_i - y[i] * k_11 * (alpha_in - alpha_io) - y[j] * k_12 * (alpha_jn - alpha_jo)
-                b_j = b - e_j - y[i] * k_12 * (alpha_in - alpha_io) - y[j] * k_22 * (alpha_jn - alpha_jo)
-
-                # if 0 < alpha_in < const:
-                #     b = b_i
-                # elif 0 < alpha_jn < const:
-                #     b = b_j
-                # else:
-                b = (b_i + b_j) / 2.0
-
-                changes += 1
-                # print('iter: %d, i=%d, changes=%d' % (it, i, changes))
-
-        if changes == 0:
-            it += 1
-        else:
-            it = 0
-
-    return alpha, b
 
 
 class SMO:
@@ -239,7 +156,7 @@ class SMO:
         # plot classification surface
         self.get_surface()
         x = np.linspace(-2, 10, 100)
-        y = (self.b - self.w[1] * x) / self.w[0]
+        y = (-self.b - self.w[0] * x) / self.w[1]
         plt.plot(x, y)
 
         plt.axis([-1, 10, -6, 5])
@@ -262,7 +179,7 @@ class SMO:
                 return True
         return False
 
-    def run_naive(self):
+    def train_naive(self):
         while self.it <= self.max_it:
             print('iter: ', self.it)
             changes = 0
@@ -270,7 +187,7 @@ class SMO:
                 self.e[i] = self.g(i) - float(self.y[i])
                 if self.y[i] * self.e[i] < -self.err and self.alpha[i] < self.const \
                         or self.y[i] * self.e[i] > self.err and self.alpha[i] > 0:
-                    j = get_random_id(i, self.m)
+                    j = random_var(i, self.m)
                     self.e[j] = self.g(j) - float(self.y[j])
                     if self.update(i, j) == 0:
                         changes += 1
@@ -282,7 +199,7 @@ class SMO:
                 self.it = 0
         return True
 
-    def run(self):
+    def train(self):
         while self.loop():
             print('iter: %d' % self.it)
 
